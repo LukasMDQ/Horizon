569c7d5654942aa2b405e423f02a3faf04b9881a
Leandro Gabriel Ahumada <mystikgames19@gmail.com>
2024-10-04 18:23:12 -0300

    Fix Animations Jewels
    Now activates a special one-time animation when picking up a jewel

--- Assets/Scripts/Drop.cs
+++ Assets/Scripts/Drop.cs
@@ -6,13 +6,14 @@ public class Drop : MonoBehaviour
 {
     public int value = default;
     [SerializeField] GameObject _effect;
-    [SerializeField] bool _ammo, _heal, _maxHp, _jewel;           
+    [SerializeField] bool _ammo, _heal, _maxHp, _jewel;
+    public CalizAnimation caliz;
     private void OnTriggerEnter(Collider other)
     {       
         if (other.TryGetComponent(out Stats stats) && other.CompareTag("Player"))//el efecto del item varia dependiendo del bool
         {  
             if(_ammo) stats.Reload(value);
-            if (_jewel) stats.AddJewel(value);
+            if (_jewel) stats.AddJewel(value); caliz.JewelUpdate();
             if (_heal) stats.Heal(value);
             if(_maxHp) stats.MaxLifeUp(value);
             Destruction();
--- Assets/Scripts/Player/CalizAnimation.cs
+++ Assets/Scripts/Player/CalizAnimation.cs
@@ -19,19 +19,6 @@ public class CalizAnimation : MonoBehaviour
 
     void Update()
     {
-        if(stats.jewels >= 1) //Setear la visibilidad dependiendo de cuantas gemas tenemos
-        {
-            jewel1.SetActive(true);
-        }
-        if (stats.jewels >= 2)
-        {
-            jewel2.SetActive(true);
-        }
-        if (stats.jewels >= 3)
-        {
-            jewel3.SetActive(true);
-        }
-
         if (Input.GetKeyDown(KeyCode.Alpha1)){ //Habilidad 1 - chequea si está en cooldown y cuantas gemas hay, preparado para más adelante si tenemos 3 anim para cada habilidad
             if(!skills.onCooldown[0] && stats.jewels >= 1) {
                 caliz.SetTrigger("Trigger");
@@ -61,6 +48,25 @@ public class CalizAnimation : MonoBehaviour
             {
                 caliz.SetTrigger("Cooldown");
             }
+        }
+    }
+
+    public void JewelUpdate()
+    {
+        if (stats.jewels == 1) //Setear la visibilidad dependiendo de cuantas gemas tenemos
+        {
+            jewel1.SetActive(true);
+            caliz.SetTrigger("Jewel1");
+        }
+        if (stats.jewels == 2)
+        {
+            jewel2.SetActive(true);
+            caliz.SetTrigger("Jewel2");
+        }
+        if (stats.jewels == 3)
+        {
+            jewel3.SetActive(true);
+            caliz.SetTrigger("Jewel3");
         }
     }
 }
