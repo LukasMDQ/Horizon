4f0aac485a72716fe0e948cda7787cd6acd8b3b9
Leandro Gabriel Ahumada <mystikgames19@gmail.com>
2024-10-04 17:16:42 -0300

    Feedback Caliz - Materials, Animations
    Animation on Cooldown, one for every pickups, changed material

--- Assets/Scripts/Player/CalizAnimation.cs
+++ Assets/Scripts/Player/CalizAnimation.cs
@@ -1,20 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CalizAnimation : MonoBehaviour
 {
     Animator caliz;
+    public Skills skills;
+    public Stats stats;
+    public GameObject jewel1;
+    public GameObject jewel2;
+    public GameObject jewel3;
+
     void Start()
     {
         caliz = GetComponent<Animator>();
     }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Alpha3)){
-            caliz.SetTrigger("Trigger");
+        if(stats.jewels >= 1) //Setear la visibilidad dependiendo de cuantas gemas tenemos
+        {
+            jewel1.SetActive(true);
+        }
+        if (stats.jewels >= 2)
+        {
+            jewel2.SetActive(true);
+        }
+        if (stats.jewels >= 3)
+        {
+            jewel3.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)){ //Habilidad 1 - chequea si está en cooldown y cuantas gemas hay, preparado para más adelante si tenemos 3 anim para cada habilidad
+            if(!skills.onCooldown[0] && stats.jewels >= 1) {
+                caliz.SetTrigger("Trigger");
+            }
+            else{
+                caliz.SetTrigger("Cooldown");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (!skills.onCooldown[1] && stats.jewels >= 2)
+            {
+                caliz.SetTrigger("Trigger");
+            }
+            else
+            {
+                caliz.SetTrigger("Cooldown");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (!skills.onCooldown[2] && stats.jewels >= 3)
+            {
+                caliz.SetTrigger("Trigger");
+            }
+            else
+            {
+                caliz.SetTrigger("Cooldown");
+            }
         }
     }
-
-
 }
--- Assets/Skills.cs
+++ Assets/Skills.cs
@@ -15,8 +15,8 @@ public class Skills : MonoBehaviour
     //  cooldown en segundos (10 seg)
     public float cooldownTime = 10f;
 
-    // habilidades  en cooldown
-    private bool[] _onCooldown;
+    // habilidades  en cooldown - public para acceder desde animaciones del Caliz
+    public bool[] onCooldown;
 
     // tiempo restante de cooldown de cada habilidad
     private float[] _cooldownTimers;
@@ -26,7 +26,7 @@ public class Skills : MonoBehaviour
 
     private void Start()
     {
-        _onCooldown = new bool[skillsObject.Length];
+        onCooldown = new bool[skillsObject.Length];
         _cooldownTimers = new float[skillsObject.Length];
         _stats = GetComponent<Stats>();
         
@@ -43,17 +43,17 @@ public class Skills : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1) && !_onCooldown[0] && _stats.jewels >= 1)
+        if (Input.GetKeyDown(KeyCode.Alpha1) && !onCooldown[0] && _stats.jewels >= 1)
         {
             ActivateSkill(0);
             _stats.Heal(10);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2) && !_onCooldown[1] && _stats.jewels >= 2)
+        if (Input.GetKeyDown(KeyCode.Alpha2) && !onCooldown[1] && _stats.jewels >= 2)
         {
             ActivateSkill(1);
             _stats.Buff(10);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha3) && !_onCooldown[2] && _stats.jewels >= 3)
+        if (Input.GetKeyDown(KeyCode.Alpha3) && !onCooldown[2] && _stats.jewels >= 3)
         {
             ActivateSkill(2);
         }
@@ -61,7 +61,7 @@ public class Skills : MonoBehaviour
         // Actualiza tiempos de cooldown
         for (int i = 0; i < skillsObject.Length; i++)
         {
-            if (_onCooldown[i])
+            if (onCooldown[i])
             {
                 _cooldownTimers[i] -= Time.deltaTime;
                 cooldownSliders[i].value = _cooldownTimers[i] / cooldownTime;  // Actualiza el valor del slider
@@ -105,12 +105,12 @@ public class Skills : MonoBehaviour
     private IEnumerator Cooldown(int index)
     {
         // Establecer la habilidad en cooldown y tiempo restante
-        _onCooldown[index] = true;
+        onCooldown[index] = true;
         _cooldownTimers[index] = cooldownTime;
 
         yield return new WaitForSeconds(cooldownTime);
 
         // Habilidad disponible 
-        _onCooldown[index] = false;
+        onCooldown[index] = false;
     }
 }
