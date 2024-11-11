9bc7bf417dd96f2c74d74617ccd052cea99960f1
Leandro Gabriel Ahumada <mystikgames19@gmail.com>
2024-11-11 17:52:55 -0300

    Discord Rich Presence Integration
    Now displays the game running in your Discord Account :D

---
+++ Assets/Plugins/Discord_Controller.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Discord;
+using System.Data;
+
+public class Discord_Controller : MonoBehaviour
+{
+    public long applicationID;
+    public string details = "Playing";
+    public string state = "";
+    public string largeImage = "icon";
+    public string largeText = "Por Encima del Horizonte";
+
+    private long time;
+    private static bool instanceExists;
+    public Discord.Discord discord;
+
+    void Awake()
+    {
+        if (!instanceExists)
+        {
+            instanceExists = true;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        discord = new Discord.Discord(applicationID, (System.UInt64)Discord.CreateFlags.NoRequireDiscord);
+
+        time = System.DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+        UpdateStatus();
+    }
+
+    void Update()
+    {
+        try
+        {
+            discord.RunCallbacks();
+        }
+        catch
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void LateUpdate()
+    {
+        UpdateStatus();
+    }
+
+    void UpdateStatus()
+    {
+        try
+        {
+            var activityManager = discord.GetActivityManager();
+            var activity = new Discord.Activity
+            {
+                Details = details,
+                State = state,
+                Assets =
+                {
+                    LargeImage = largeImage,
+                    LargeText = largeText,
+                },
+                Timestamps =
+                {
+                    Start = time
+                }
+            };
+
+            activityManager.UpdateActivity(activity, (res) =>
+            {
+                if (res != Discord.Result.Ok) Debug.LogWarning("Failed Connecting to Discord!");
+            });
+        }
+        catch
+        {
+            Destroy(gameObject) ;
+        }
+    }
+}
