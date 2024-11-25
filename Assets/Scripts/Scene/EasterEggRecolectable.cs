b5725235127cd7c72c7262d5fe219f595ccaeb60
Leandro Gabriel Ahumada <mystikgames19@gmail.com>
2024-11-25 17:12:57 -0300

    First Level
    Tutorial Level finished

--- Assets/Scripts/Scene/EasterEggRecolectable.cs
+++ Assets/Scripts/Scene/EasterEggRecolectable.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class EasterEggRecolectable : MonoBehaviour, IInteractable
 {
+    public AudioSource source;
+    public AudioClip clip;
     void IInteractable.Interact()
     {
         PlayerStatsManager.easterEggAcc++;
+        source.PlayOneShot(clip);
         Debug.Log($"Player stats easter eggs {PlayerStatsManager.easterEggAcc}");
         gameObject.SetActive(false);
         TooltipSystem.Hide();
