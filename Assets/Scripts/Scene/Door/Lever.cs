ada95c099b8de7194d4b09dbadc7e8bd8dd338b1
GotyLP <88514207+GotyLP@users.noreply.github.com>
2024-12-25 23:29:09 -0300

    blocked lever close door action in levels

--- Assets/Scripts/Scene/Door/Lever.cs
+++ Assets/Scripts/Scene/Door/Lever.cs
@@ -8,7 +8,16 @@ public class Lever : MonoBehaviour, IInteractable
     public event Action OnLeverActivated;
     public event Action OnLeverDeactivated;
 
+    public AudioSource audioSource;
+    [SerializeField]
+    private AudioClip blockSoundEffect;
+    [SerializeField]
+    private AudioClip activateSoundEffect;
+    [SerializeField]
+    private AudioClip deactivateSoundEffect;
     private bool isActivated = false;
+    [SerializeField]
+    private bool isBlockedDeactivated = false;
 
     public Animator leverAnimator;
 
@@ -22,11 +31,19 @@ public class Lever : MonoBehaviour, IInteractable
             isActivated = true;
             OnLeverActivated?.Invoke();
             leverAnimator.SetTrigger(LeverActivated);
+            if (activateSoundEffect) audioSource.PlayOneShot(activateSoundEffect);
         } else
         {
-            isActivated = false;
-            OnLeverDeactivated?.Invoke();
-            leverAnimator.SetTrigger(LeverDeactivated);
+            if (!isBlockedDeactivated)
+            {
+                isActivated = false;
+                OnLeverDeactivated?.Invoke();
+                leverAnimator.SetTrigger(LeverDeactivated);
+                if (deactivateSoundEffect) audioSource.PlayOneShot(deactivateSoundEffect);
+            } else
+            {
+                if (blockSoundEffect) audioSource.PlayOneShot(blockSoundEffect);
+            }           
         }
     }
 }
