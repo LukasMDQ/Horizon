27a365f5a391f3ea6b5718c004fbbcf5c774d4ee
GotyLP <lidiamportiz@gmail.com>
2025-08-31 14:46:17 -0300

    fixed pausemenu

--- Assets/Scripts/UI/Pause.cs
+++ Assets/Scripts/UI/Pause.cs
@@ -1,68 +1,71 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-// TP 2 - Ahumada, Leandro
-// Script de Menú de Pausa, Frenar y Reanudar.
 
 public class Pause : MonoBehaviour
 {
     public GameObject pauseMenu;
     public GameObject devMenu;
+    public GameObject hpBossUI;
     public KeyCode toggleKey = KeyCode.F1;
-    public bool isPaused;   
-    void Start() //Siempre que entra a un nivel, el menú de Pausa está desactivado.
+    public bool isPaused;
+
+    void Start()
     {
         pauseMenu.SetActive(false);
-       
+        Time.timeScale = 1f;
     }
 
-    void Update() //Pausa
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (isPaused)
-            {
-                ResumeGame();
-            }
-            else
-            {
-                PauseGame();
-            }
+            if (isPaused) ResumeGame();
+            else PauseGame();
         }
+
         if (isPaused)
         {
             DevMode();
         }
     }
 
-    public void PauseGame() //Pausa el juego.
+    public void PauseGame()
     {
-        
-        pauseMenu.SetActive(true);
         Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         isPaused = true;
-        
+
+        if(hpBossUI != null )
+        {
+            hpBossUI.SetActive(false);
+        }
     }
-    void DevMode() //Menu desarrollador
+
+    void DevMode()
     {
         if (Input.GetKeyDown(toggleKey))
         {
             devMenu.SetActive(!devMenu.activeSelf);
-        }       
+        }
     }
 
-    public void ResumeGame() //Reanuda el Juego.
+    public void ResumeGame()
     {
-        pauseMenu.SetActive(false);
         Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         isPaused = false;
+
+        if (hpBossUI != null)
+        {
+            hpBossUI.SetActive(true);
+        }
     }
-    public void ResumeGameFromMenu() //Método especial que se ejecuta cuando se vuelve al menú principal. (Reanuda el juego desde la pausa, mantiene el cursor visible y desbloqueado)
+
+    public void ResumeGameFromMenu()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
